db08950961e6cd316b2023f0b1cf5168ecdb3381
Bhuvaneswari1 <50100445+Bhuvaneswari1@users.noreply.github.com>
2019-05-01 10:33:34 +0530

    Update hunter_set_8.py

--- hunter_set_8.py
+++ hunter_set_8.py
@@ -1,8 +1,8 @@
 n=input()
 a=list(map(int,input().split()))
 for i in range(0,len(a)):
-    for j in range(1,len(a)):
-        for k in range(2,len(a)):
+    for j in range(i+1,len(a)):
+        for k in range(j+1,len(a)):
             if(i<j<k):
                 if(a[i]+a[j]==a[k]):
                     print(a[i],end=" ")
