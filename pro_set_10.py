19c06710be09014317557af719acc58ed9857156
Bhuvaneswari1 <50100445+Bhuvaneswari1@users.noreply.github.com>
2019-05-12 17:12:36 +0530

    Create pro_set_10.py

---
+++ pro_set_10.py
@@ -0,0 +1,8 @@
+num=int(input())
+steps=list(map(int,input().split()))
+total=0
+for i in range(1,len(steps)):
+    for j in range(0,i):
+        if(steps[j]<steps[i]):
+            total+=steps[j]
+print(total)
