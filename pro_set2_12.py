309e54aca67ec13ae51a91dca776659845fb484d
Bhuvaneswari1 <50100445+Bhuvaneswari1@users.noreply.github.com>
2019-05-22 15:54:50 +0530

    Create pro_set2_12.py

---
+++ pro_set2_12.py
@@ -0,0 +1,8 @@
+n,q=map(int,input().split())
+z=list(map(int,input().split()))
+arr=[]
+for i in range(q):
+    u,v=map(int,input().split())
+    arr.append(sum(z[u-1:v]))
+for i in range(len(arr)):
+    print(arr[i])
